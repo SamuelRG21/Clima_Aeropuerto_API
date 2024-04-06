ec59f56d615b2f2e704a0aafcc44a3102d974254
SamuelRG21 <samg115lo@gmail.com>
2024-04-06 13:06:26 -0600

    Eliminar carpeta de proyecto sin gitignore

--- webAplication/Models/Message.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace webAplication.Models
-{
-    public class Message
-    {
-        public int Code { get; set; }
-        public string? Status { get; set; }
-        public string? Description { get; set; }
-    }
-}
